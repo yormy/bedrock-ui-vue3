3d41c5abad4ba311b801b063302b9d013c3cf1d5
rob <rob@prigroup.eu>
2022-11-11 14:07:48 +0100

    AlertText

--- src/components/Atom/Alert/Text/AlertText.vue
+++ src/components/Atom/Alert/Text/AlertText.vue
@@ -1,50 +1,51 @@
-<script>
-import YPurifiedHtml from '../../Display/PurifiedHtml.vue';
-
+<script setup lang="ts">
 /**
  * Show an alertbox
  *
  * Cases:
  *  - error messages, success messages
  */
-export default {
-    inheritAttrs: false,
+import YPurifiedHtml from '../../Display/PurifiedHtml.vue';
+import {computed} from "vue";
 
-    components: {
-        YPurifiedHtml,
+const props = defineProps({
+    /**
+     * The message of the alert, displayed in the header
+     */
+    message: {
+        type: String,
+        default: '',
+        required: false,
     },
 
-    props: {
-        /**
-         * The message of the alert, displayed in the header
-         */
-        message: {
-            type: String,
-            default: '',
-            required: false,
-        },
-
-        /**
-         * The styling o the modal is-info , is-success, is-warning, is-danger
-         */
-        styling: {
-            type: String,
-            default: 'is-danger'
-        }
+    /**
+     * The styling o the modal is-info , is-success, is-warning, is-danger
+     */
+    styling: {
+        type: String,
+        default: 'is-danger'
     },
 
-    computed: {
-        styleClass() {
-            return this.alertBorder + ' ' + this.styling;
-        }
+    /**
+     * The styling ?
+     */
+    alertBorder: {
+        type: String,
+        default: 'is-danger'
     }
-}
+});
+
+
+const styleClass = computed<string>(() => {
+    return props.alertBorder + ' ' + props.styling;
+});
+
 </script>
 
 <template>
     <div>
         <div v-if="message" :class="styleClass" role="alert">
-            <y-purified-html :value="message"></y-purified-html>
+            <YPurifiedHtml :value="message"></YPurifiedHtml>
         </div>
     </div>
 </template>
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import ComponentB from './ComponentB.vue';
 import ComponentC from './ComponentC.vue';
 
 import PurifiedHtml from './Atom/Display/PurifiedHtml.vue';
+import YAlertText from './Atom/Alert/Text/AlertText.vue';
 
-export { ComponentA, ComponentB, ComponentC, PurifiedHtml };
+export { ComponentA, ComponentB, ComponentC, PurifiedHtml, YAlertText};
