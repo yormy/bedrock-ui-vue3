c2eb76b148500b0dc11f6d44e035ea555b5e61c8
rob <rob@prigroup.eu>
2022-11-11 12:30:27 +0100

    init alert component

---
+++ src/components/Atom/Alert/Text/AlertTextDanger.vue
@@ -0,0 +1,18 @@
+<script>
+import YAlertText from './AlertText.vue'
+
+export default {
+    components: {
+        YAlertText,
+    },
+}
+</script>
+
+<template>
+    <y-alert-text
+        class="is-danger"
+        v-bind="{...$props, ...$attrs}"
+        v-on="$listeners"
+    >
+    </y-alert-text>
+</template>
