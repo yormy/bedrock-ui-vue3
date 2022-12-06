e9e7ab5e6cdffe2dbf3cbb01c515c247edf3f670
rob <dev@mexion.com>
2022-12-06 19:22:41 +0100

    primary secondary button

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,10 +4,13 @@
 import ComponentA from './ComponentA.vue';
 import ComponentB from './ComponentB.vue';
 import ComponentC from './ComponentC.vue';
+//
+// // @ts-ignore
+// export * from './Atom/Alert/exports.js';
+// // @ts-ignore
+// export * from './Atom/Display/exports.js';
 
 // @ts-ignore
-export * from './Atom/Alert/exports.js';
-// @ts-ignore
-export * from './Atom/Display/exports.js';
+export * from './buttons/exports.js';
 
 export { ComponentA, ComponentB, ComponentC };
