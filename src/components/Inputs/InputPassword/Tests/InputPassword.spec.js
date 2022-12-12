45c3bf7ba0a89c8b60983fc50e0f705891dac070
rob <dev@mexion.com>
2022-12-12 20:23:01 +0100

    add tests

---
+++ src/components/Inputs/InputPassword/Tests/InputPassword.spec.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import { mount } from '@vue/test-utils';
+import { h } from 'vue';
+import InputEmail from '../InputPassword.vue';
+
+describe('InputEmail.vue', () => {
+    test.skip('ddd', () => {});
+    // it('should render InputEmail', () => {
+    //     const wrapper = mount(InputEmail, {
+    //         provide: () => ({
+    //             i18ntc: 'i18ntc'        // to prevent warnings because there was no provider in the testset
+    //         })
+    //     });
+    //
+    //     expect(wrapper.html()).toContain('p-inputtext');
+    // });
+});
