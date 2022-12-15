e9977b6061deccde178be32730ebd2b1b0132ccd
rob <dev@mexion.com>
2022-12-15 16:20:28 +0100

    select language dropdown

---
+++ docs/components/select/language.md
@@ -0,0 +1,9 @@
+<script setup>
+import {YSelectLanguage} from 'bedrock-ui-vue3'
+</script>
+
+# Select Language
+
+<DemoContainer>
+  <YSelectLanguage :right-aligned="true"></YSelectLanguage>
+</DemoContainer>
--- src/components/Selects/Language/SelectLanguage.vue
+++ src/components/Selects/Language/SelectLanguage.vue
@@ -1,57 +1,44 @@
 <template>
-    <div>
-        dd
         <div class="y-dropdown--container">
             <!-- Dropdown toggle -->
-            <input id="checkbox-125" type="checkbox" class="hidden y-dropdown--checkbox--input" >
-            <label for="checkbox-125" class="flex justify-end" >
+            <input id="checkbox-125" type="checkbox" class="hidden y-dropdown--checkbox--input " >
+            <label for="checkbox-125" class="flex" :class="labelClass" >
                 <div  class="y-dropdown--label">
                     <span class="flag-icon flag-icon-de"></span>
                 </div>
-                <div class="y-icon icon icon-dropdown"></div>
+                <div class="y-icon icon icon-dropdown "></div>
             </label>
 
             <!-- Dropdown card -->
-            <div class="y-dropdown--card right-aligned">
+            <div class="y-dropdown--card" :class="dropdownClass">
                 <a href="#" class="y-dropdown--item"> <span class="flag-icon flag-icon-nl"></span> Dutch  </a>
                 <a href="#" class="y-dropdown--item"> <span class="flag-icon flag-icon-us"></span> English </a>
             </div>
         </div>
-    </div>
 </template>
 
 <script setup lang="ts">
 import PrimeMenubar from 'primevue/menubar';
-import {ref} from "vue";
+import {defineProps, computed} from "vue";
+import useButtonTypes from "../../Buttons/useButtonTypes";
 
-const items = ref([
-    {
-        label:'File',
-        icon:'y-icon icon icon-users',
-        items:[
-            {
-                label:'New',
-                icon:'y-icon icon icon-users',
-                items:[
-                    {
-                        label:'Bookmark',
-                        icon:'pi pi-fw pi-bookmark'
-                    },
-                    {
-                        label:'Video',
-                        icon:'pi pi-fw pi-video'
-                    }
-                ]
-            },
-            {
-                label:'Delete',
-                icon:'pi pi-fw pi-trash'
-            },
-            {
-                separator:true
-            }
-        ]
+const props = defineProps({
+    rightAligned: {
+        type: Boolean,
+        default: true,
     },
-]);
+    leftAligned: {
+        type: Boolean,
+        default: true,
+    }
+});
+
+const labelClass = computed(() => {
+    return props.rightAligned ? 'justify-content-end' : 'justify-content-start'
+})
+
+const dropdownClass = computed(() => {
+    return props.rightAligned ? 'right-aligned' : ''
+})
 
 </script>
