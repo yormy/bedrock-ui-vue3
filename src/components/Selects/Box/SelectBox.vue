eef6c6d781cae48aa152edb41d4201b09a4004ea
rob <dev@mexion.com>
2022-12-16 09:56:07 +0100

    WIP validation

--- src/components/Selects/Box/SelectBox.vue
+++ src/components/Selects/Box/SelectBox.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="field-checkbox">
-        <prime-select-box inputId="binary" v-model="checked" v-bind="$attrs" :binary="true" @click="handleInputChanged"/>
+        {{ v$.checkedValue.$model}}
+        <prime-select-box inputId="binary" v-model="v$.checkedValue.$model" v-bind="$attrs" :binary="true" @click="handleInputChanged"/>
+{{v$.checkedValue.$errors}}
         <y-icon-required :required="required"></y-icon-required>
         <label for="binary">
             <y-purified-html :value="moreHelpDescription"></y-purified-html>
@@ -16,16 +18,29 @@
 </template>
 
 <script setup lang="ts">
-import {defineProps, ref, defineEmits, watch, nextTick} from 'vue';
+import {defineProps, ref, defineEmits, watch, nextTick, reactive} from 'vue';
 import PrimeSelectBox from 'primevue/checkbox';
 import YIconHelp from '../../Helpers/IconHelp.vue';
 import YIconRequired from '../../Helpers/IconRequired.vue';
 import YPurifiedHtml from '../../Helpers/PurifiedHtml.vue';
 
+import {required, sameAs, minLength, email} from '@vuelidate/validators';
+import {useVuelidate} from "@vuelidate/core";
+
 const moreHelpHeader = "heading of modal";
 const moreHelpDescription = "Hello<strong>kkk</strong> sss<a href='#'>link</a> normla <br>dsfsfsd <br>dsfsfsd"
 
 const emit = defineEmits(['isOn', 'isOff', 'update:modelValue']);
+
+const state = reactive({
+    checkedValue: false,
+});
+
+
+const rules = {
+    checkedValue: {required, sameAs: sameAs(true) },
+};
+
 
 const props = defineProps({
     modelValue: {
@@ -67,6 +82,16 @@ watch(
 async function handleInputChanged(e: Event) {
     await nextTick()
     emit('update:modelValue', checked.value);
+    handleInputBlur();
+};
+
+const v$ = useVuelidate(rules, state);
+
+v$.value.checkedValue.$touch(); // validate on loading
+
+const handleInputBlur = () => {
+    console.log('touch');
+    v$.value.checkedValue.$touch();
 };
 
 </script>
