2a237c460a59082f3dd44775f1a83fe28d32a77a
rob <dev@mexion.com>
2022-12-21 15:44:41 +0100

    add tags

--- docs/components/tags/tags.md
+++ docs/components/tags/tags.md
@@ -8,6 +8,9 @@ The default button to press
 
 <DemoContainer>
   <div class="flex justify-content-between">
-      <YTag label="Primary"/>
+      <YTag label="watch out" icon="y-icon icon icon-file-download" type="is-danger-open" />
+
   </div>
 </DemoContainer>
+
+<YTag label="watch " class="mr-2" value="ssss" rounded/>
--- src/components/Tags/Base/TagBase.vue
+++ src/components/Tags/Base/TagBase.vue
@@ -1,20 +1,79 @@
 <template>
-    <prime-tag value="taggie"></prime-tag>
+    <span class="y-tag-chip" :class="labelClass">
+        <span v-if="icon" :class="icon"></span>{{ label }}
+    </span>
 </template>
 
 <script setup lang="ts">
-import PrimeTag from 'primevue/tag';
-// import PrimeSplitButton from 'primevue/splitbutton';
-// import {computed, defineProps, watch} from 'vue';
-// import useButtonTypes from '../useButtonTypes';
-// import useBadgeTypes from '../useBadgeTypes';
-// import useButtonSizes from '../useButtonSizes';
-//
-// const props = defineProps({
-//     type: {
-//         type: String,
-//         default: '',
-//     },
+import {computed} from "vue";
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: 'dsfdsfds',
+    },
+
+    icon: {
+        type: String,
+        default: '',
+    },
+
+    shape: {
+        type: String,
+        validator(value: string) {
+            return [
+                'rounded',
+                'square',
+            ].includes(value)
+        }
+    },
+
+    size: {
+        type: String,
+        validator(value: string) {
+            return [
+                'tiny',
+                'small',
+                'normal',
+                'large',
+                'extra-large',
+            ].includes(value)
+        }
+    },
+
+    type: {
+        type: String,
+        default: 'is-info-open',
+        validator(value: string) {
+            return [
+                'is-info-open',
+                'is-info-closed',
+                'is-success-open',
+                'is-success-closed',
+                'is-warning-open',
+                'is-warning-closed',
+                'is-danger-open',
+                'is-danger-closed',
+                'is-disabled-open',
+                'is-disabled-closed'
+            ].includes(value)
+        }
+    },
+});
+
+const labelClass = computed(() => {
+    return [
+        props.shape,
+        props.size,
+        props.type,
+    ];
+})
+
+
+
+
+
+
 //     badgeType: {
 //         type: String,
 //         default: '',
