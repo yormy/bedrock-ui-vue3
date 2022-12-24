34e84ea241c06cc9c8f23fd5fe828d2be9203e9d
rob <dev@mexion.com>
2022-12-24 11:47:43 +0100

    toggle icon / section

--- src/components/Selects/Toggle/Base/ToggleBase.vue
+++ src/components/Selects/Toggle/Base/ToggleBase.vue
@@ -1,12 +1,36 @@
 <template>
     <div class="y-content">
-        <label class="y-toggle-slider wide-slider" for="toggle-123" >
+        todo: sqaure vs round round = yellow
+        colors not correct...
+        <label class="y-toggle-slider round wide-slider" for="toggle-123" >
             <input id="toggle-123" type="checkbox" checked class="hidden checkbox-input">
             <span class="is-off red">No</span>
             <span class="is-on green">Yes</span>
             <span class="slider">&nbsp;</span>
         </label>
+
+
+        <br>--<br>
+        <label class="y-toggle-section" for="toggle-125" >
+            <span class="y-faded-small">
+                <input id="toggle-125" type="checkbox" checked class="hidden checkbox-input">
+                <span class="is-off"><span class="y-icon icon icon-darkmode"> *******</span></span>
+                <span class="is-on"><span class="y-icon icon icon-lightmode"> ######</span></span>
+            </span>
+        </label>
+
+        <br>--<br>
+        <label class="y-toggle-section" for="toggle-1251" >
+            <input id="toggle-1251" type="checkbox" checked class="hidden checkbox-input">
+            <span class="is-off"><span class="y-icon icon icon-darkmode"> *******</span></span>
+            <span class="is-on"><span class="y-icon icon icon-lightmode"> ######</span></span>
+        </label>
+
     </div>
+
+
+
+
 </template>
 
 <script setup lang="ts">
