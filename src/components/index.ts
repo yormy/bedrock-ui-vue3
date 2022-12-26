b4230298d4eec5597e76280772ac5de9bd7814f9
rob <dev@mexion.com>
2022-12-26 15:01:22 +0100

    moved to atoms dir

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,57 +1,5 @@
 /* eslint-disable import/extensions */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
-import ComponentA from './ComponentA.vue';
-import ComponentB from './ComponentB.vue';
-import ComponentC from './ComponentC.vue';
-//
-// // @ts-ignore
-// export * from './Atom/Alert/exports.js';
-// // @ts-ignore
-// export * from './Atom/Display/exports.js';
-
 // @ts-ignore
-export * from './Buttons/exports.js';
-// @ts-ignore
-export * from './Inputs/exports.js';
-// @ts-ignore
-export * from './Modals/exports.js';
-
-// @ts-ignore
-export * from './Selects/exports.js';
-
-// @ts-ignore
-export * from './Helpers/exports.js';
-
-// @ts-ignore
-export * from './Blocks/exports.js';
-
-// @ts-ignore
-export * from './Layouts/exports.js';
-
-// @ts-ignore
-export * from './Tags/exports.js';
-
-// @ts-ignore
-export * from './Misc/exports.js';
-
-// @ts-ignore
-export * from './Messages/exports.js';
-
-// @ts-ignore
-export * from './Toasts/exports.js';
-
-// @ts-ignore
-export * from './Cards/exports.js';
-
-// @ts-ignore
-export * from './Tables/exports.js';
-
-// @ts-ignore
-export * from './Tabs/exports.js';
-
-// @ts-ignore
-export * from './Dropdowns/exports.js';
-
-// @ts-ignore
-export * from './Display/exports.js';
+export * from './Atoms/exports.js';
