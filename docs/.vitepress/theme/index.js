9796f830431f86f44f04bdef5a825cae15236b7a
rob <dev@mexion.com>
2022-12-26 17:31:00 +0100

    use ds theme

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -13,14 +13,15 @@ import 'primeflex/primeflex.css'
 import './custom.css'
 
 import Layout from '../components/Layout.vue'
-import DarkTheme from './dark-theme.css'
+// import DarkTheme from './dark-theme.css'
 
-import '../../../src/assets/themes/bedrock-indigo/theme.css';
+import '../../../src/assets/themes/ds-indigo/theme.css';
+//import '../../../src/assets/themes/bedrock-indigo/theme.css';
 
 import '../../../resources/icons/fontawesome-pro6/css/all.css';
 import '../../../resources/public/shared/css/icons/icons.css';
-
-export { DarkTheme }
+//
+// export { DarkTheme }
 
 import i18ntc from './i18ntc';
 
